83e3b4b3932c9ece5693b7b33ddb01a12eeb4c21
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-10-30 13:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  console.log(string)
+function logShout('HELLO') {
+  console.log('HELLO')
 }
 
 function logWhisper(string) {
