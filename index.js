68f13f8864250bb1338c2c8cc8ffdca087447564
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-10-30 13:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === 'hello'){
-    return `I cant hear you!`
+    return `I can\'t hear you!`
   } else if (string === 'HELLO') {
     return `YES INDEED`
   } else if (string === 'I love you, Grandma.') {
