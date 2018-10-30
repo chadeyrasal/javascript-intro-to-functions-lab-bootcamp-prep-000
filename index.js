196bc2925e63b1f71187204e57b91d08ac1df571
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-10-30 13:37:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+var uppercase = 'HELLO'
+
+function logShout(uppercase) {
+  console.log(uppercase)
+}
+
+function logWhisper(string) {
+
+}
+
+function sayHiToGrandma(string) {
+
+}
